b21e9ebf055191c71a5fbf95e46f770cbda5d797
James R. McKaskill <jmckaskill@gmail.com>
2009-07-06 08:47:51 +1200

    Adding sha1 lua binding

---
+++ sha1-0.2/sha1lua.cpp
@@ -0,0 +1,33 @@
+#include "sha1.h"
+
+extern "C" {
+#include <lua.h>
+#include <lauxlib.h>
+}
+
+static int sha1_digest(lua_State* L)
+{
+  SHA1 sha;
+  size_t size;
+  const char* data = luaL_checklstring(L, 1, &size);
+  sha.addBytes(data, size);
+
+  unsigned char* digest = sha.getDigest();
+  int digestlen = 20;
+  lua_pushlstring(L, (const char*) digest, digestlen);
+  return 1;
+}
+
+
+static const luaL_Reg kReg[] = {
+  {"digest", &sha1_digest},
+  {NULL, NULL},
+};
+
+extern "C" int luaopen_sha1(lua_State* L);
+
+int luaopen_sha1(lua_State* L)
+{
+  luaL_register(L, "sha1", kReg);
+  return 1;
+}
