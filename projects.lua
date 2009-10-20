51e5b07ed3861f5fee12f5c67c90e973b58028c2
James McKaskill <jmckaskill@trimble.co.nz>
2009-10-20 19:04:12 +1300

    Changes to projects.lua

--- projects.lua
+++ projects.lua
@@ -1,9 +1,13 @@
+local function directory(name)
+  files {name .. "/**.c", name .. "/**.cpp", name .. "/**.h"}
+end
+
 local function common(name, type, id)
   project(name)
   uuid(id)
   kind(type)
   flags {"ExtraWarnings", "FatalWarnings"}
-  files {name .. "/**.c", name .. "/**.cpp", name .. "/**.h"}
+  directory(name)
   defines {"ADBUS_LITTLE_ENDIAN"}
   configuration {"windows"}
     defines {"LUA_BUILD_AS_DLL", "ADBUS_BUILD_AS_DLL"}
@@ -41,28 +45,27 @@ c99_project('adbus', 'SharedLib', "6A120200-37D4-FA48-9838-B343A13D98C1")
 cpp_project("adbuscpp", "StaticLib", "903F096F-1B59-1546-93BA-9295493C7AA4")
   defines {"ADBUSCPP_LIBRARY"}
 
-c99_project('adbuslua_core', "SharedLib", "EE40C960-59A7-C849-92F2-14024DC8181A")
+c99_project('adbuslua', "SharedLib", "EE40C960-59A7-C849-92F2-14024DC8181A")
+  targetname 'adbuslua_core'
   defines {"ADBUSLUA_LIBRARY"}
-  links {"adbus", "lua"}
+  links {"adbus", "lua5.1"}
   configuration {"windows"}
     links {"ws2_32", "advapi32", "user32"}
 
-ansi_project("lua", "SharedLib", "84C1D0B6-011F-B046-A5AA-EE30E0B8D214")
-  targetname "lua5.1"
+ansi_project("lua5.1", "SharedLib", "84C1D0B6-011F-B046-A5AA-EE30E0B8D214")
   defines {"LUA_LIBRARY"}
-  files {"lua-5.1.4/src/**.h", "lua-5.1.4/src/**.c"}
+  directory 'lua-5.1.4/src'
   excludes {"lua-5.1.4/src/lua.c", "lua-5.1.4/src/luac.c"}
   configuration "linux"
     links {"dl", "m", "readline"}
 
-ansi_project("luaexec", "ConsoleApp", "A1031290-BB35-CA4C-8F9E-F099DAA91ABA")
-  targetname "lua"
+ansi_project("lua", "ConsoleApp", "A1031290-BB35-CA4C-8F9E-F099DAA91ABA")
   files {"lua-5.1.4/src/lua.c"}
-  links {"lua", "adbuslua_core"}
+  links {"lua5.1", "adbuslua"}
 
 c99_project("luaxml", "SharedLib", "9B52ED3C-AA46-5843-81F3-4FFA3C6EADED")
   targetname "LuaXML_lib"
-  links "lua"
+  links "lua5.1"
 
 c99_project("test-adbus", "ConsoleApp", "F05DC97B-044A-1847-AC92-8E39F3A7C10D")
   files {"adbus/vector.c",
